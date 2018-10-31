d43c5ea2841132914372541b2ab37610e0d35a1c
xiongjian <webjsforyou@gmail.com>
2018-10-31 11:17:39 +0800

    注册路由

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## git操作
+## 1、 git操作
 ### 远程仓库有内容（公司有功能）
 * 将远程仓库内容克隆到本地来  
   * git clone url
@@ -11,7 +11,7 @@
   * 提交远程仓库去
     * git push origin dev  
     
-## 本地仓库有内容
+### 本地仓库有内容
 * 新建远程仓库
   * 去github新建一个仓库
 * 本地仓库进行管理
@@ -29,14 +29,33 @@
   * 提交远程仓库去
     * git push origin dev  
     
-## 问题：远程仓库有两分支master和dev
+### 问题：远程仓库有两分支master和dev
 * 如果通过克隆只能获取master分支上的内容
 * 我需要dev分支的内容：
   * git checkout -b dev origin/dev  将远程仓库dev分支克隆到本地dev分支上
   
-## 合并分支操作
+### 合并分支操作
 * 切换到master分支
   * git checkout master
 * 合并dev分支
-  * git merge dev  
+  * git merge dev 
+
+## 2、 项目准备
+*  项目描述: 整体业务功能/功能模块/主体的技术/开发模式
+*  技术选型: 数据展现/用户交互/组件化, 后端, 前后台交互, 模块化, 项目构建/工程化, 其它
+*  API接口: 接口的4个组成部分, 接口文档, 对/调/测接口
+   
+
+## 3、 搭建项目
+* 使用create-react-app脚手架创建模板项目(工程化)
+* 引入antd-mobile, 并实现按需打包和自定义主题
+* 引入react-router-dom(v4): 
+	* HashRouter/Route/Switch
+    * history: push()/replace()
+* 引入redux
+	* redux/react-redux/redux-thunk
+	* redux: createStore()/combineReducers()/applyMiddleware()
+	* react-redux: <Provider store={store}> / connect()(Xxx)
+	* 4个重要模块: reducers/store/actions/action-types
+  
   
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,9 +1,94 @@
 import React, {Component} from 'react';
+import {NavBar, List, InputItem, Button, WingBlank, WhiteSpace, Radio} from 'antd-mobile';
+
+import Logo from '../logo';
+
+const Item = List.Item;
 
 class Register extends Component {
+  state = {
+    username: '',
+    password: '',
+    rePassword: '',
+    type: 'laoban'
+  }
+  
+  /*setUsername = val => {
+    //更新状态
+    this.setState({
+      username: val
+    })
+  }
+  
+  setPassword = val => {
+    this.setState({
+      password: val
+    })
+  }*/
+  
+  handleChange = (name, val) => {
+    //更新状态
+    this.setState({
+      [name]: val
+    })
+  }
+  
+  register = () => {
+    //获取表单数据
+    const {username, rePassword, password, type} = this.state;
+    console.log(username, rePassword, password, type);
+    //发送ajax请求
+  }
+  
+  goLogin = () => {
+    //跳转到登录路由，编程式导航
+    this.props.history.replace('/login');  //替换浏览历史记录
+  }
+  
   render () {
+    const {type} = this.state;
     return (
-      <h2>register</h2>
+      <div>
+        <NavBar>硅 谷 直 聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <form>
+            <List>
+              <WhiteSpace />
+              <InputItem placeholder="请输入用户名" onChange={val => this.handleChange('username', val)}>用户名：</InputItem>
+              <WhiteSpace />
+              <InputItem
+                placeholder="请输入密码"
+                type="password"
+                onChange={val => this.handleChange('password', val)}
+              >密码：</InputItem>
+              <WhiteSpace />
+              <InputItem
+                placeholder="请输入确认密码"
+                type="password"
+                onChange={val => this.handleChange('rePassword', val)}>确认密码：</InputItem>
+              <WhiteSpace />
+              <Item>
+                用户类型： &nbsp;&nbsp;
+                <Radio
+                  className="my-radio"
+                  checked={type === 'dashen'}
+                  onClick={() => this.handleChange('type', 'dashen')}
+                >大神</Radio> &nbsp;&nbsp;&nbsp;&nbsp;
+                <Radio
+                  className="my-radio"
+                  checked={type === 'laoban'}
+                  onClick={() => this.handleChange('type', 'laoban')}
+                >老板</Radio>
+              </Item>
+              <WhiteSpace />
+              <Button type="primary" onClick={this.register}>注 册</Button>
+              <WhiteSpace />
+              <Button onClick={this.goLogin}>已有账户</Button>
+            </List>
+          </form>
+        </WingBlank>
+      </div>
     )
   }
 }
